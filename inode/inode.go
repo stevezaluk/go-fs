74f006ded76bf73bda954608a3b0055183f94be0
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-29 20:35:15 -0500

    Added an additional field describing the type of inode (under inodeType)

--- inode/inode.go
+++ inode/inode.go
@@ -4,6 +4,11 @@ import (
 	"time"
 )
 
+const (
+	I_FILE = 0
+	I_DIR  = 1
+)
+
 /*
 IndexNode - Represents an Index Node containing metadata about the file.
 Associated metadata includes its inode number (a unique identifier) for the index
@@ -15,6 +20,7 @@ Uniquely, a file's name is not stored in and inode and is only stored by its dir
 type IndexNode struct {
 	inodeNum  int64
 	inodeSize int64
+	inodeType int8
 
 	creationTime *time.Time
 	modifiedTime *time.Time
@@ -29,12 +35,13 @@ NewIndexNode - Constructor for creating an index node. An inode number
 is unaware of the inode table and what inode number should be allocated
 next.
 */
-func NewIndexNode(inodeNum int64) *IndexNode {
+func NewIndexNode(inodeNum int64, inodeType int8) *IndexNode {
 	timestamp := time.Now()
 
 	return &IndexNode{
 		inodeNum:     inodeNum,
 		inodeSize:    0,
+		inodeType:    inodeType,
 		creationTime: &timestamp,
 		modifiedTime: &timestamp,
 		accessTime:   &timestamp,
