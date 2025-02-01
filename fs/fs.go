7674a8f713a967ad83131cdcbfc5bd518b5bfce7
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-31 21:11:02 -0500

    Added a constructo for the file system object

--- fs/fs.go
+++ fs/fs.go
@@ -1,3 +1,18 @@
 package fs
 
+import "github.com/stevezaluk/go-fs/block"
 
+/*
+FileSystem - A representation of the file system as a whole. Contains
+traditional system calls that you would expect to find in a filesystem
+*/
+type FileSystem struct {
+	SuperBlock *block.SuperBlock
+}
+
+/*
+NewFileSystem - A constructor for the file system object
+*/
+func NewFileSystem() *FileSystem {
+	return &FileSystem{SuperBlock: block.NewSuperBlock()}
+}
